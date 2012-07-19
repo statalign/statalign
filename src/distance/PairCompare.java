4f82d83bb33243bd0cef03c350e7f260ef8aa238
ingolfur <ingolfur@ingolfurLab>
2012-07-19 17:04:43 +0100

    Distance added

---
+++ src/distance/PairCompare.java
@@ -0,0 +1,12 @@
+package distance.distance;
+
+import java.util.Comparator;
+
+public class PairCompare implements Comparator < Pair <String,Integer> >  {
+
+	@Override
+	public int compare(Pair<String,Integer>  o1, Pair<String,Integer>  o2) {
+		return o1.getLeft().compareTo(o2.getLeft());
+	}
+
+}
