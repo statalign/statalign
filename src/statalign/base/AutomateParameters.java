7776d8409daf1bcdccecda742c115f71ec67dd45
Ingolfur Edvardsson <ingolfured@gmail.com>
2012-08-23 19:47:50 +0100

    Doc-ed the automateParameters

--- src/statalign/base/AutomateParameters.java
+++ src/statalign/base/AutomateParameters.java
@@ -5,6 +5,16 @@ import java.util.ArrayList;
 
 import statalign.postprocess.utils.RNAFoldingTools;
 
+/**
+ * A class that contains the logic when to stop the burn-in, sampling, or 
+ * how to determine the sampling rate. 
+ * 
+ * 
+ * @author ingolfur
+ *
+ */
+
+
 public class AutomateParameters {
 	
 
@@ -14,7 +24,21 @@ public class AutomateParameters {
 	private static boolean automateNumberOfSamplesToTake = false;
 	private static boolean automateBurnIn = true;
 	
-
+	//steprate
+	final static double decline = 0.0001;
+	final static int right = 10;
+	final static int wrong = 2;
+	final static int total = right + wrong;
+	final static double errorForTheAVG = 0.99;
+	
+	//stopsampling
+	final static double PERCENT_CONST = 0.999;
+	
+	//burn-in
+	final static int DECLINE = 30;
+	final static int NUTR_SIZE = 100;
+	
+	
 	public static boolean shouldAutomateBurnIn() {
 		return automateBurnIn;
 	}
@@ -39,14 +63,18 @@ public class AutomateParameters {
 	public static void setAutomateNumberOfSamples(boolean set) {
 		automateNumberOfSamplesToTake = set;
 	}
+	
+	/**
+	 * Looks either for major decline in theSpace or if we get very close to the average line. Then we break.
+	 * 
+	 * @param theSpace 				Gives you the average similarity between samples that have index * stepSizeOfTheBurnIn
+	 * 								steps between them (theSpace[2] gives you the average similarity between samples
+	 * 								that are 2 * stepSizeOfTheBurnIn steps away)
+	 * @param stepSizeOfTheBurnIn   only used in the end, maybe unnecessary ?
+	 * @return						the desired sampling rate (int)
+	 */
 
-	public static int getSampleRateOfTheSpace(ArrayList<Double> theSpace, int stepSizeOfTheBurnIn2) {
-		final double decline = 0.0001;
-		final int right = 10;
-		final int wrong = 2;
-		final int total = right + wrong;
-		final double error = 0.99;
-		
+	public static int getSampleRateOfTheSpace(ArrayList<Double> theSpace, int stepSizeOfTheBurnIn) {
 		ArrayList<Double> der = new ArrayList<Double>(theSpace.size()-1);
 		for(int i = 1; i<theSpace.size()-1; i++){
 			der.add(theSpace.get(i)-theSpace.get(i+1));
@@ -68,15 +96,22 @@ public class AutomateParameters {
 			for(int k = 0; k<total; ++k){
 				isUprising += (der.get(i+k) < decline) ? 1 : 0;
 			}
-			if (isUprising >= right || f * error <= avg){
-				return i*stepSizeOfTheBurnIn2;
+			if (isUprising >= right || f * errorForTheAVG <= avg){
+				return i*stepSizeOfTheBurnIn;
 			}
 		}
-		return 1;
+		return 50;
 	}
 	
+	/**
+	 * If the last element in the distances variable is larger than PERCENT_CONST
+	 * we stop.
+	 * 
+	 * @param distances 	ArrayList of distances between consecutive fuzzyAlignments
+	 * @return 				True if we want to stop, else false
+	 */
+	
 	public static boolean shouldStopSampling(ArrayList<Double> distances){
-		double PERCENT_CONST = 0.999;
 		if(distances.size() < 2){
 			return false;
 		}
@@ -87,9 +122,13 @@ public class AutomateParameters {
 	}
 
 	
+	/**
+	 * If the logLikelihood has a major decline, we stop the burnin
+	 * 
+	 * @param logLikeList   List of the values where we look for the decline
+	 * @return				true if we found a major decline in the logLikeList and we want to stop sampling, else false
+	 */
 	public static boolean shouldStopBurnIn(ArrayList<Double> logLikeList){
-		final int DECLINE = 30;
-		final int NUTR_SIZE = 100;
 		if(logLikeList.size() < (NUTR_SIZE + DECLINE)){
 			return false;
 		}
