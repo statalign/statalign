c03456fca165cc6f146b1443079d52f30991b95a
cjchallis <christopher.challis@gmail.com>
2012-10-17 15:07:43 -0400

    Added function to calculate covariance between all leaves of tree.

--- src/statalign/model/ext/plugins/StructAlign.java
+++ src/statalign/model/ext/plugins/StructAlign.java
@@ -24,6 +24,10 @@ public class StructAlign extends ModelExtension {
 	/** covariance matrix implied by current tree topology */
 	double[][] fullCovar;
 	
+	/** parameters of structural drift */
+	double theta;
+	double sigma2;
+	
 	@Override
 	public double logLikeFactor(Tree tree) {
 		// simplest (and slowest) approach: get alignment of all leaves and compute likelihood from there
@@ -63,7 +67,7 @@ public class StructAlign extends ModelExtension {
 		
 		// create normal distribution with mean 0 and covariance subCovar
 		MultivariateNormalDistribution multiNorm = new MultivariateNormalDistribution(new double[numMatch], subCovar);
-		
+	
 		double li = 1;
 		double[] vals = new double[numMatch];
 		// loop over all 3 coordinates
@@ -77,7 +81,7 @@ public class StructAlign extends ModelExtension {
 
 	/**
 	 * extracts the specified rows and columns of a 2d array
-	 * @param matrix, 2d array from which to extract; rows, rows to extract; cols, cols to extract
+	 * @param matrix, 2d array from which to extract; rows, rows to extract; cols, columns to extract
 	 * @return submatrix
 	 */
 	public double[][] getSubMatrix(double[][] matrix, int[] rows, int[] cols) {
@@ -89,13 +93,74 @@ public class StructAlign extends ModelExtension {
 	}
 	
 	/**
-	 * return the full covariance matrix for the tree toplogy and branch lenghts
+	 * return the full covariance matrix for the tree topology and branch lengths
 	 */	
 	public double[][] calcFullCovar(Tree tree) {
-		// TODO
-		 double[][] x = new double[5][6];
-		 return x;
+		// I'm assuming that tree.names.length is equal to the number of vertices here
+		double[][] distMat = new double[tree.names.length][tree.names.length];
+		calcDistanceMatrix(tree.root, distMat);
+		for(int i = 0; i < tree.names.length; i++)
+			for(int j = i; j < tree.names.length; j++)
+				distMat[i][j] = sigma2 / (2 * theta ) * (1 - Math.exp(2 * theta * distMat[i][j]));
+		for(int i = 0; i < tree.names.length; i++)
+			for(int j = i + 1; j < tree.names.length; j++)
+				distMat[j][i] = distMat[i][j];
+		return distMat;
 	}
+	
+	/**
+	 * recursive algorithm to traverse tree and calculate distance matrix between leaves 
+	 */		
+	public int[] calcDistanceMatrix(Vertex vertex, double[][] distMat){
+		int[] subTree = new int[distMat.length];
+		// either both left and right are null or neither is
+		if(vertex.left != null){
+			int[] subLeft  = calcDistanceMatrix(vertex.left, distMat);
+			int[] subRight = calcDistanceMatrix(vertex.right, distMat);
+			int i = 0;
+			while(subLeft[i] > -1){
+				subTree[i] = subLeft[i];
+				i++;
+			}
+			for(int j = 0; subRight[j] > -1; j++)
+				subTree[i+j] = subRight[j];
+		}
+		else{
+			subTree[0] = vertex.leafCount;
+			for(int j = 0; j < distMat.length; j++)
+				subTree[j] = -1;
+		}
+		addEdgeLength(distMat, subTree, vertex.edgeLength);
+			
+		return subTree;
+	}
+		
+	// adds the length of the current edge to the distance between all leaves
+	// of a subtree to all other leaves
+	// 'rows' contains the indices of vertices in the subtree
+	public void addEdgeLength(double[][] distMat, int[] subTree, double edgeLength){
+		int i = 0;
+		while(subTree[i] > -1){
+			for(int j = 0; j < subTree.length; j++)  
+				distMat[subTree[i]][j] = distMat[subTree[i]][j] + edgeLength;
+			i++;
+		}
+			
+		// edge length should not be added to distance between vertices in the subtree
+		// subtract the value from these entries of the distance matrix
+		i = 0;
+		while(subTree[i] > -1){
+			int j = 0;
+			while(subTree[j] > -1){
+				distMat[subTree[i]][subTree[j]] = 
+						distMat[subTree[i]][subTree[j]] - edgeLength;
+				j++;
+			}
+			i++;
+		}
+	}
+
+
 	
 	@Override
 	public int getParamChangeWeight() {
