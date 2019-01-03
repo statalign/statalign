32bcad4a0329c1b5da501ec4ec778f28950afcec
1j <herman@stats.ox.ac.uk>
2019-01-03 05:17:13 -0500

    Removing vestigial build/config scripts.

--- build-jar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-version=$( cat release_version.txt | sed 's/v//' )
-pushd $DIR > /dev/null
-    gradle clean build shadowJar -DmainClass=statalign.StatAlign -DbaseName=StatAlign
-    cp build/libs/StatAlign-$version-all.jar ./StatAlign.jar
-popd > /dev/null
